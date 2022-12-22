f84f6075c534319d132b30bb1522b80e780c3eed
Hou, Han <houhan@gmail.com>
2022-12-22 12:06:00 -0800

    WIP: filtered widget persistent across pages

--- app/streamlit_util.py
+++ app/streamlit_util.py
@@ -3,6 +3,14 @@ import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode, ColumnsAutoSizeMode, DataReturnMode
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+import matplotlib.pyplot as plt
+import numpy as np
 
 custom_css = {
 ".ag-root.ag-unselectable.ag-layout-normal": {"font-size": "15px !important",
@@ -101,3 +109,111 @@ def aggrid_interactive_table_units(df: pd.DataFrame):
     )
 
     return selection
+
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    
+    # modify = st.checkbox("Add filters")
+
+    # if not modify:
+    #     return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+    
+    def cache_widget(field):
+        st.session_state[f'{field}_cache'] = st.session_state[field]
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns,
+                                                            default=st.session_state.to_filter_columns_cache 
+                                                                    if 'to_filter_columns_cache' in st.session_state
+                                                                    else [],
+                                                            key='to_filter_columns',
+                                                            on_change=cache_widget,
+                                                            args=['to_filter_columns'])
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 30:
+                selected = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=st.session_state[f'select_{column}_cache']
+                            if f'select_{column}_cache' in st.session_state
+                            else list(df[column].unique()),
+                    key=f'select_{column}',
+                    on_change=cache_widget,
+                    args=[f'select_{column}']
+                )
+                df = df[df[column].isin(selected)]
+                
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                
+                # fig, ax = plt.subplots()
+                # ax.hist(df[column], bins=100)
+                # st.pyplot(fig)
+                # counts, bins = np.histogram(df[column], bins=100)
+                # st.bar_chart(pd.DataFrame(
+                #                 {'x': bins[1:], 'y': counts},
+                #                 ),
+                #                 x='x', y='y')
+                
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value= st.session_state[f'select_{column}_cache']
+                            if f'select_{column}_cache' in st.session_state
+                            else (_min, _max),
+                    step=step,
+                    key=f'select_{column}',
+                    on_change=cache_widget,
+                    args=[f'select_{column}']
+                )
+                df = df[df[column].between(*user_num_input)]
+                
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
